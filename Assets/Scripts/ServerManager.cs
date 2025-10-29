39e05f693dc6b2596b5076a563aed70b3c83670c
shrimp <a0920460509@gmail.com>
2025-10-29 16:18:17 +0800

    webglcontroll_switch)raw image

--- Assets/Scripts/ServerManager.cs
+++ Assets/Scripts/ServerManager.cs
@@ -8,6 +8,7 @@ using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 using System.Threading.Tasks;
 using Erolabs.Sdk.Unity;
+using Games.Coresdk.Unity;
 using Unity.VisualScripting;
 
 
@@ -740,10 +741,27 @@ public class ServerManager : MonoBehaviour
 
         await tcs.Task;
     }
-
+    private Language DetectSystemLanguage()
+    {
+        switch (Application.systemLanguage)
+        {
+            case SystemLanguage.ChineseSimplified:
+                return Language.cn;
+            case SystemLanguage.ChineseTraditional:
+                return Language.zh;
+            case SystemLanguage.Japanese:
+                return Language.jp;
+            case SystemLanguage.Korean:
+                return Language.ko;
+            case SystemLanguage.English:
+            default:
+                return Language.en;
+        }
+    }
     private async UniTask HandleErolabsLoginAsync()
     {
-        await ErolabsSDK.Initialize();
+        Language lang = DetectSystemLanguage();
+        await ErolabsSDK.Initialize(null, lang);
         Debug.Log("Erolabs SDK initialized");
 
         bool loginCompleted = false;
